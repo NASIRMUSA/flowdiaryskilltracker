e8dfc0d3d8496fd430eaec546bf15b56dfe3e9eb
nasirmusa <nasirmusanigeria@gmail.com>
2025-05-23 11:41:10 +0000

    update

--- sw.js
+++ sw.js
@@ -3,8 +3,6 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
